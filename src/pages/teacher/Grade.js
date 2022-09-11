50041ff56ce9c9745182529f5be7c75db5220a54
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-11 05:58:16 +0300

    functionality: Adding the teacher grading table

--- src/pages/teacher/Grade.js
+++ src/pages/teacher/Grade.js
@@ -1,24 +1,68 @@
 //doesn'st work - cascades errors
-import React from "react"
+import React, { useState } from "react"
 
 function Grade({loggedInUser}){   
-      
+  const [selectedAllocationId,setSelectedAllocationId] = useState(0)
+
+  const allRegistrations = loggedInUser.allocations.map(allocation => {
+    return allocation.course.registrations //for each allocated course return the course and its registrations
+  }).flat(1)
+
+  const displayedRegistrations = allRegistrations.filter(registration => registration.course_id ===parseInt(selectedAllocationId) )
+ 
+function handleOnchange(event){
+  setSelectedAllocationId(event.target.value)
+}
+
 
 
     return(          
       <div className="studentDashboard">
       <h1>Grade Your Students </h1> 
-      <div id="summaries">
+      <div>
       <div className="selectCourseToGrade">
-        <label>Allocated Course:</label><select className="selector">
-        <option selected>Select coourse to grade</option>
-        {loggedInUser.allocations.map(allocation=><option>{allocation.course.course_name}</option>)}
+        <label>Allocated Course:</label><select className="selector" onChange={handleOnchange} defaultValue={selectedAllocationId}>
+        <option value={0}>Select course to grade</option>
+        {loggedInUser.allocations.map(allocation=><option key={allocation.id} value={allocation.course_id}>{allocation.course.course_name}</option>)}
         </select>
         </div>
-       
-      </div>
-      <h1>Allocation details</h1>
-      
+       <div>
+        {/* <ul>
+          {displayedRegistrations.map(registration=><li key={registration.id}>{registration.course_id}</li>)}
+        </ul> */}
+
+        <form>
+        <table className="table">
+      <thead>
+         <tr>
+            <th>Course Name</th>
+            <th>Student Name</th>
+            <th>Student Id</th>
+            <th>Assessment Grade</th>
+         </tr>
+      </thead>
+      <tbody>         
+     {displayedRegistrations.map(registration=>
+     <tr key={registration.id}>
+      <td>{registration.course_name}</td>
+      <td>{registration.student_name}</td>      
+      <td>{registration.student_id}</td>
+      <td>
+        <select className="gradeSelect">
+         <option value="missing">Select</option>
+          <option value="A">A</option>
+          <option value="B">B</option>
+          <option value="C">C</option>
+          <option value="D">D</option>
+          <option value="E">E</option>
+        </select>
+      </td>
+      </tr>)}
+      </tbody>
+   </table>   
+        </form>
+       </div>
+      </div> 
       </div> 
     )
 
